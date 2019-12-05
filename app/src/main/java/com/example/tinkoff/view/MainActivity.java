2c1618918958d7cfbb6d8bd81e0849f025c21206
alexdevyatov <alexeidevyatov94@gmail.com>
2019-12-05 19:12:22 +0400

    Добавил pull-to-refresh

--- app/src/main/java/com/example/tinkoff/view/MainActivity.java
+++ app/src/main/java/com/example/tinkoff/view/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.example.tinkoff.App;
 import com.example.tinkoff.R;
@@ -19,9 +22,11 @@ import com.example.tinkoff.viewmodel.factory.NewsListViewModelFactory;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+    private SwipeRefreshLayout swipeRefreshLayout;
     private NewsFeedAdapter adapter;
     private NewsListViewModel newsListViewModel;
 
@@ -31,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView);
+        progressBar = findViewById(R.id.progressBar);
+        swipeRefreshLayout = findViewById(R.id.swipeRefresh);
+        swipeRefreshLayout.setOnRefreshListener(this);
         List<News> newsList = new ArrayList<>();
         adapter = new NewsFeedAdapter(newsList);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
@@ -41,11 +49,19 @@ public class MainActivity extends AppCompatActivity {
     private void createViewModel() {
         AppComponent appComponent = ((App)getApplication()).getAppComponent();
         newsListViewModel = ViewModelProviders.of(this, new NewsListViewModelFactory(appComponent)).get(NewsListViewModel.class);
+        progressBar.setVisibility(View.VISIBLE);
         newsListViewModel.request();
         newsListViewModel.getData().observe(this, this::updateNews);
     }
 
     private void updateNews(NewsFeed newsFeed) {
+        progressBar.setVisibility(View.GONE);
         adapter.updateNewsList(newsFeed.getNewsList());
+        swipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void onRefresh() {
+        newsListViewModel.request();
     }
 }
